0a2a5e3eb32c52f3a4e176246c77c697bb0d37a0
Julien SENON <julien.senon@gmail.com>
2019-02-04 00:18:51 +0100

    Add middleware Header

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,10 @@ use rustracing::tag::Tag;
 
 use actix_web::{
     error, http, middleware, server, App, AsyncResponder, Error, HttpMessage,
-    HttpRequest, HttpResponse, dev::Handler
+    HttpRequest, HttpResponse, Result
 };
+use actix_web::middleware::{Middleware, Started, Response};
+use http::{header, HttpTryFrom};
 
 use bytes::BytesMut;
 use futures::{Future, Stream};
@@ -27,6 +29,34 @@ struct MyObj {
     name: String,
     age: i32,
 }
+
+struct Headers;  // <- Our middleware
+
+/// Middleware implementation, middlewares are generic over application state,
+/// so you can access state with `HttpRequest::state()` method.
+impl<S> Middleware<S> for Headers {
+
+    /// Method is called when request is ready. It may return
+    /// future, which should resolve before next middleware get called.
+    fn start(&self, _: &HttpRequest<S>) -> Result<Started> {
+        Ok(Started::Done)
+    }
+
+    /// Method is called when handler returns response,
+    /// but before sending http message to peer.
+    fn response(&self, _: &HttpRequest<S>, mut resp: HttpResponse)
+        -> Result<Response>
+    {
+        resp.headers_mut().insert(
+            header::HeaderName::try_from("X-VERSION").unwrap(),
+            header::HeaderValue::from_static("0.1.0"));
+        resp.headers_mut().insert(
+            header::HeaderName::try_from("X-APP-NAME").unwrap(),
+            header::HeaderValue::from_static("Sample-Actix-server"));
+        Ok(Response::Done(resp))
+    }
+}
+
 
 
 fn index(_: &HttpRequest) -> String {
@@ -55,7 +85,12 @@ fn post_user(req: &HttpRequest) -> Box<Future<Item = HttpResponse, Error = Error
         .responder()
 }
 
-fn span(_: &HttpRequest) -> String {
+fn span(req: &HttpRequest) -> HttpResponse {
+    let r = req.clone();
+    let view = r.headers().get("X-Custom-Id");
+    
+    println!("Header: {:?}", view);
+
     let time : u64 = 5;
     let (tracer, span_rx) = Tracer::new(AllSampler);
     std::thread::spawn(move || {
@@ -86,16 +121,21 @@ fn span(_: &HttpRequest) -> String {
             std::thread::sleep(Duration::from_millis(time));
         }
     }
-    format!("Span generated, Stay {:?}ms sleeping", time)
+    let data = format!("Span generated, stay {}ms sleeping", time);
+    HttpResponse::Ok()
+        .content_type("plain/text")
+        .header("X-Hdr", "sample")
+        .body(data)
 }
 
 fn main() {
     ::std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
-    let sys = actix::System::new("sample-actix-servers");
+    let sys = actix::System::new("sample-actix-server");
     server::new(|| {
         App::new()
             .middleware(middleware::Logger::default())
+            .middleware(Headers)
             .resource("/user", |r| r.method(http::Method::POST).f(post_user))
             .resource("/", |r| r.method(http::Method::GET).f(index))
             .resource("/span", |r| r.method(http::Method::GET).f(span))
